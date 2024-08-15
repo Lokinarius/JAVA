b948fd6025c4629a2776d8042fa0eb3f21d39a6e
William Galvão <Liam_draven@outlook.com>
2024-08-15 19:19:21 -0300

    Tipos primitivos

---
+++ FIASCO/Java Geral/001 - Tipos Primitivos/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Tipos primitivos */
+
+        // Toda variável em Java deve ter o ponto e vírgula no final ';'
+        int idade = 28; // 32 bits
+        double altura = 1.80; // 16 bits
+        char sexo = 'M'; // 16 bits
+        Long id = 2847412412444421L; // 64 bits
+        // O int armazena no máximo até 2147483647, após esse valor faz se necessário a utilização do Long
+        // O Long sempre possui um 'L' no final do número
+    }
+}
