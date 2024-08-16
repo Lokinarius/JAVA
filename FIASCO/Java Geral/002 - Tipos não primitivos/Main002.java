3715f126629dc86988e8b3c48ca2b6c57ac2435b
William Galvão <Liam_draven@outlook.com>
2024-08-16 17:01:52 -0300

    Tipos não primitivos

--- FIASCO/Java Geral/002 - Tipos não primitivos/Main002.java
+++ FIASCO/Java Geral/002 - Tipos não primitivos/Main002.java
@@ -1,3 +1,12 @@
 public class Main002 {
-    
+    public static void main(String[] args) {
+        /* Tipos não primitivos */
+        // Permite maiores níveis de alteração
+
+        String nome = "William";
+
+        System.out.println("Meu nome é " + nome);
+
+
+    }
 }
