a75148be6f8e8511e169b7c463202f8a32f3fc7f
William Galvão <Liam_draven@outlook.com>
2024-08-16 17:02:12 -0300

    Métodos e Parametros

---
+++ FIASCO/Java Geral/003 - Métodos e Parametros/Main003.java
@@ -0,0 +1,11 @@
+public class Main003 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            meuNome("William", 28);
+
+        }
+    }
+    private static void meuNome(String nome, int idade){
+        System.out.println("Meu nome é " + nome + " e minha idade é " + idade + " anos.");
+    }
+}
