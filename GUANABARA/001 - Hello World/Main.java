095c412d3cbfabb49cbec06c949920c692320903
William Galvão <Liam_draven@outlook.com>
2024-08-19 16:26:48 -0300

    update Hello World

--- GUANABARA/001 - Hello World/Main.java
+++ GUANABARA/001 - Hello World/Main.java
@@ -4,3 +4,17 @@ public class Main {
     }
 }
 
+/* Camel Case */
+// No Java se utiliza o Camel Case
+// MinhaClasse : Classes já possuem a primeira letra da primeira palavra em maiusculo
+// meuAtributo : Atributos possuem a primeira letra primeira letra em maiúsculo a partir da segunda palavra
+// meu_pacote : palavras com somente letras em minusculo são pacotes
+// MINHA_CONSTANTE : Palavra com somente letras em maiusculo são consideradas constantes
+
+/*
+package main; --> Pacote
+public class Main { --> Classe
+    public static void main(String[] args) { --> Métod0
+        System.out.println("Hello World!");
+    }
+}*/
