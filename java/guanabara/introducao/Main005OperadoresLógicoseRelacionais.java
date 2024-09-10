137fd1e751d3b2a998ea054b9e0f027b5e32c244
William Galvão <Liam_draven@outlook.com>
2024-09-10 14:24:12 -0300

    Update Main005OperadoresLógicoseRelacionais.java

--- java/guanabara/introducao/Main005OperadoresLógicoseRelacionais.java
+++ java/guanabara/introducao/Main005OperadoresLógicoseRelacionais.java
@@ -5,15 +5,32 @@ public class Main005OperadoresLógicoseRelacionais {
         // Operadores lógicos e relacionais
         boolean a = true;
         boolean b = false;
+        int c = 5;
+        int d = 3;
 
-        System.out.println("Operadores lógicos e relacionais");
+        System.out.println("Operadores lógicos");
         System.out.println("AND / E");
         System.out.println("a && b = " + (a && b)); // false
         System.out.println("OR / OU");
         System.out.println("a || b = " + (a || b)); // true
         System.out.println("NÃO / NEGADO");
         System.out.println("!a = " + (!a)); // false
-        System.out.println("Relacionais");
-        
+        System.out.println("\n");
+
+        System.out.println("Operadores Relacionais");
+        System.out.println("Maior que");
+        System.out.println("c > d = " + (c > d)); // false
+        System.out.println("Menor que");
+        System.out.println("c < d = " + (c < d)); // true
+        System.out.println("Maior ou igual");
+        System.out.println("c >= d = " + (c >= d)); // false
+        System.out.println("Menor ou igual");
+        System.out.println("c <= d = " + (c <= d)); // true
+        System.out.println("Igual");
+        System.out.println("c == d = " + (c == d)); // false
+        System.out.println("Diferente");
+        System.out.println("c!= d = " + (c!= d)); // true
+
+
     }
 }
