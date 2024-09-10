7ea95cbf1ac63ba9967dc7ae75a8cacbed14599d
William Galvão <Liam_draven@outlook.com>
2024-09-10 14:20:02 -0300

    Create Main005OperadoresLógicoseRelacionais.java

---
+++ java/guanabara/introducao/Main005OperadoresLógicoseRelacionais.java
@@ -0,0 +1,19 @@
+package java.guanabara.introducao;
+
+public class Main005OperadoresLógicoseRelacionais {
+    public static void main(String[] args) {
+        // Operadores lógicos e relacionais
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("Operadores lógicos e relacionais");
+        System.out.println("AND / E");
+        System.out.println("a && b = " + (a && b)); // false
+        System.out.println("OR / OU");
+        System.out.println("a || b = " + (a || b)); // true
+        System.out.println("NÃO / NEGADO");
+        System.out.println("!a = " + (!a)); // false
+        System.out.println("Relacionais");
+        
+    }
+}
