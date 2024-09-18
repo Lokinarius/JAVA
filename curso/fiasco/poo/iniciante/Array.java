536b3eaa25334f7581f1dd25041d347e63cbd043
William Galvão <Liam_draven@outlook.com>
2024-09-18 14:59:00 -0300

    Update Array.java

--- curso/fiasco/poo/iniciante/Array.java
+++ curso/fiasco/poo/iniciante/Array.java
@@ -1,4 +1,15 @@
 package curso.fiasco.poo.iniciante;
 
 public class Array {
+    public static void main(String[] args) {
+        String[] ninja = new String[3];
+        //int[] idade = new int[3];
+
+        ninja[0] = "Orochimaru";
+        ninja[1] = "Jiraya";
+        ninja[2] = "Tsunade";
+
+        System.out.println(ninja[0]);
+
+    }
 }
