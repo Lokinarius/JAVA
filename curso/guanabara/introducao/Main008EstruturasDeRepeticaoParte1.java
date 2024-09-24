ccd537f0fe3b69602599f1bab9e5abf353aaf7d7
William Galvão <Liam_draven@outlook.com>
2024-09-24 15:48:16 -0300

    Create Main008EstruturasDeRepeticaoParte1.java

---
+++ curso/guanabara/introducao/Main008EstruturasDeRepeticaoParte1.java
@@ -0,0 +1,14 @@
+package curso.guanabara.introducao;
+
+public class Main008EstruturasDeRepeticaoParte1 {
+    public static void main(String[] args) {
+
+        // ESTRUTURA DE REPETIÇÃO WHILE
+        int j = 0;
+        System.out.println("while (j < 10)");
+        while (j < 10) {
+            System.out.println("Iteração " + (j + 1));
+            j++;
+        }
+    }
+}
