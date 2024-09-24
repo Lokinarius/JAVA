6c08c6b83bb5656586d3fe3995a4e405ed12d4c8
William Galvão <Liam_draven@outlook.com>
2024-09-24 15:48:24 -0300

    Create Main010EstruturasDeRepeticaoParte3.java

---
+++ curso/guanabara/introducao/Main010EstruturasDeRepeticaoParte3.java
@@ -0,0 +1,12 @@
+package curso.guanabara.introducao;
+
+public class Main010EstruturasDeRepeticaoParte3 {
+    public static void main(String[] args) {
+        // ESTRUTURA COM VARIÁVEL DE CONTROLE
+        int numero = 10;
+        for (int i = 0; i < numero; i++) {
+            System.out.println("NÚMERO: " + (i + 1));
+        }
+    }
+}
+
