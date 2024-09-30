a22ee04a0f529afff828d3d9cf6c837024989b78
William Galvão <Liam_draven@outlook.com>
2024-09-30 17:36:20 -0300

    Create xyz.java

---
+++ curso/guanabara/introducao/Prova/xyz.java
@@ -0,0 +1,12 @@
+package curso.guanabara.introducao.Prova;
+
+public class xyz {
+    public static void main(String[] args) {
+        int x, y;
+        double z;
+        x = 5;
+        y = 2;
+        z = x/y;
+        System.out.println(z);
+    }
+}
