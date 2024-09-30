8dc072a50ef1af18cfe1ea2af1dd46c86aa1b430
William Galvão <Liam_draven@outlook.com>
2024-09-30 17:36:35 -0300

    Create Main013teste.java

---
+++ curso/guanabara/introducao/Prova/Main013teste.java
@@ -0,0 +1,10 @@
+package curso.guanabara.introducao.Prova;
+    public class Main013teste {
+        public static void main(String[] args) {
+            int potencia = (int) Math.pow(8,2);
+            System.out.println("O resultado é : " + potencia);
+        }
+    }
+
+
+
