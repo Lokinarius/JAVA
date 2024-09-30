97316aaaae74aa0292b3adcab52a1061e83d7e57
William Galvão <Liam_draven@outlook.com>
2024-09-30 17:36:10 -0300

    Create abc.java

---
+++ curso/guanabara/introducao/Prova/abc.java
@@ -0,0 +1,10 @@
+package curso.guanabara.introducao.Prova;
+
+public class abc {
+    public static void main(String[] args) {
+        Object[] str = {"a", "ab", "abc"};
+        for (int i = 0; i < str.length; i++) {
+            System.out.println("str["+i+"] = " + str[i]);
+        }
+    }
+}
