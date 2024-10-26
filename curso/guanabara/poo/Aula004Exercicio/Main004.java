1bb2f578508ec89329e969513c272dd2dd392ebd
William Galvão <Liam_draven@outlook.com>
2024-10-26 15:17:07 -0300

    Update Main004.java

--- curso/guanabara/poo/Aula004Exercicio/Main004.java
+++ curso/guanabara/poo/Aula004Exercicio/Main004.java
@@ -2,18 +2,37 @@ package curso.guanabara.poo.Aula004Exercicio;
 
 public class Main004 {
     public static void main(String[] args) {
+        // [CONTAS]
+        // Criando uma conta
         ContaBanco cb1 = new ContaBanco();
         cb1.setNumConta(123456);
-        cb1.setTipo("Conta Corrente");
-        cb1.setDono("João");
+        cb1.setDono("Harumi Suzuki");
+        cb1.abrirConta("Conta Corrente");
         cb1.setSaldo(1000.0f);
         cb1.setStatus(true);
+        // Criando uma segunda conta
+        ContaBanco cb2 = new ContaBanco();
+        cb2.setNumConta(789012);
+        cb2.setDono("Makoto Hisashi");
+        cb2.abrirConta("Conta Poupança");
 
-        System.out.println("Conta #" + cb1.getNumConta());
-        System.out.println("Tipo: " + cb1.getTipo());
-        System.out.println("Dono: " + cb1.getDono());
-        System.out.println("Saldo: " + cb1.getSaldo());
-        System.out.println("Status: " + cb1.isStatus());
+        //[
+        // Depósito
+        cb1.depositar(300);
+        cb2.depositar(500);
+        // Saque
+        cb1.sacar(200);
+        cb2.sacar(800);
+        // Pagamento de Mensalidade
+        cb1.pagarMensalidade();
+        cb2.pagarMensalidade();
+
+        // Estado da Conta
+        cb1.estadoAtual();
+        cb2.estadoAtual();
+
+
+
 
     }
 }
