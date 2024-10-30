944a6d33e1fa9121b57a1bee1492dcdb35de0f6a
William Galvão <Liam_draven@outlook.com>
2024-10-30 17:27:46 -0300

    Update Main005.java

--- curso/guanabara/poo/Aula005Encapsulamento/Main005.java
+++ curso/guanabara/poo/Aula005Encapsulamento/Main005.java
@@ -2,6 +2,16 @@ package curso.guanabara.poo.Aula005Encapsulamento;
 
 public class Main005 {
     public static void main(String[] args) {
-
+        ControleRemoto cr = new ControleRemoto(50,true,false);
+        cr.ligar();
+        cr.desligar();
+        cr.maisVolume();
+        cr.menosVolume();
+        cr.play();
+        cr.pause();
+        cr.abrirMenu();
+        cr.fecharMenu();
+        cr.ligarMudo();
+        cr.desligarMudo();
     }
 }
