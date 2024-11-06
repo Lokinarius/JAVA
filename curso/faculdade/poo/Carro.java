4cd00c181c816e9650ee1ef097c1560c87f79fe8
William Galvão <Liam_draven@outlook.com>
2024-11-06 16:44:56 -0300

    Create Carro.java

---
+++ curso/faculdade/poo/Carro.java
@@ -0,0 +1,32 @@
+package curso.faculdade.poo;
+
+public class Carro extends Veiculo implements Manutencao {
+    // [ATRIBUTOS]
+    private int numeroPortas;
+
+    // [CONSTRUTOR]
+    public Carro(String placa, int ano, int numeroPortas) {
+        super(placa, ano);
+        this.numeroPortas = numeroPortas;
+    }
+
+    // [METODOS]
+    @Override
+    public double calcularValorSeguro() {
+        return 500 + (numeroPortas * 100);
+    }
+
+
+    @Override
+    public String realizarManutencao() {
+        return "Manutenção realizada no carro de " + getNumeroPortas() + " portas, Modelo: " + getAno() + ", de placa" +
+                " : " + getPlaca();
+    }
+
+    // [GETTERS E SETTERS]
+    public int getNumeroPortas() {
+        return numeroPortas;
+    }
+
+
+}
