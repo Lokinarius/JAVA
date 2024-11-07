11833c8d6e4296604685b19f11ee09f4c43b01f1
William Galvão <Liam_draven@outlook.com>
2024-11-07 16:38:13 -0300

    Update MainGerenciadorVeiculos.java

--- curso/faculdade/poo/MainGerenciadorVeiculos.java
+++ curso/faculdade/poo/MainGerenciadorVeiculos.java
@@ -1,7 +1,24 @@
 package curso.faculdade.poo;
 
+import java.util.ArrayList;
+
 public class MainGerenciadorVeiculos {
     public static void main(String[] args) {
+        ArrayList<Veiculo> veiculos = new ArrayList<>();
+        veiculos.add(new Carro("SAD3456",1999,4));
+        veiculos.add(new Carro("DOW0984",2008,2));
+        veiculos.add(new Carro("ABC1234",2010,5));
+        veiculos.add(new Moto("KNT0927",2021,200));
+        veiculos.add(new Moto("ABC5678",2020,150));
+        veiculos.add(new Moto("KIL2734",2005,290));
 
+        for (Veiculo veiculo : veiculos){
+            veiculo.exibirDetalhes();
+            double valorSeguro = veiculo.calcularValorSeguro();
+            System.out.println("Valor do Seguro: " + valorSeguro);
+            Manutencao manutencao = (Manutencao) veiculo;
+            System.out.println(manutencao.realizarManutencao());
+        }
+        System.out.println("--------------------------");
     }
 }
