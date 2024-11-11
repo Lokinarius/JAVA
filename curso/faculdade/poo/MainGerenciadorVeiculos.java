92e60f8cdeb99afc17375aec0d5ea7c7f10a92cf
William Galvão <Liam_draven@outlook.com>
2024-11-11 16:49:15 -0300

    Update MainGerenciadorVeiculos.java

--- curso/faculdade/poo/MainGerenciadorVeiculos.java
+++ curso/faculdade/poo/MainGerenciadorVeiculos.java
@@ -1,9 +1,11 @@
 package curso.faculdade.poo;
 
+//[IMPORT]
 import java.util.ArrayList;
 
 public class MainGerenciadorVeiculos {
     public static void main(String[] args) {
+        //[FROTA DE VEÍCULOS]
         ArrayList<Veiculo> veiculos = new ArrayList<>();
         veiculos.add(new Carro("SAD3456",1999,4));
         veiculos.add(new Carro("DOW0984",2008,2));
@@ -12,6 +14,7 @@ public class MainGerenciadorVeiculos {
         veiculos.add(new Moto("ABC5678",2020,150));
         veiculos.add(new Moto("KIL2734",2005,290));
 
+        //[SAÍDA DE DADOS]
         for (Veiculo veiculo : veiculos){
             veiculo.exibirDetalhes();
             double valorSeguro = veiculo.calcularValorSeguro();
