94ec9ee3fe7514ded8e5b5e8af2859552cff8664
William Galvão <Liam_draven@outlook.com>
2024-12-05 18:58:12 -0300

    Update Livro.java

--- curso/guanabara/poo/Aula008ExercicioPratico/domain/Livro.java
+++ curso/guanabara/poo/Aula008ExercicioPratico/domain/Livro.java
@@ -1,52 +1,68 @@
 package curso.guanabara.poo.Aula008ExercicioPratico.domain;
 
-public class Livro {
+public class Livro implements Publicacao {
     // [ATRIBUTOS]
     private String titulo;
     private String autor;
     private int totPaginas;
     private int pagAtual;
     private boolean aberto;
-    private String leitor;
+    private Pessoa leitor;
 
     // [METODOS]
-    public void detalhes(){
-
+    public String detalhes() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", totPaginas=" + totPaginas +
+                ", pagAtual=" + pagAtual +
+                ", aberto=" + aberto +
+                ", leitor=" + leitor +
+                '}';
     }
 
     // [CONTRUTOR]
-    public Livro(String titulo, String autor, int totPaginas, int pagAtual, boolean aberto, String leitor) {
+    public Livro(String titulo, String autor, int totPaginas, int pagAtual, boolean aberto, Pessoa leitor) {
         this.titulo = titulo;
         this.autor = autor;
         this.totPaginas = totPaginas;
-        this.pagAtual = pagAtual;
-        this.aberto = aberto;
+        this.pagAtual = 0;
+        this.aberto = false;
         this.leitor = leitor;
     }
 
     // [GETTERS E SETTERS]
-
     public String getTitulo() {return titulo;}
-
     public void setTitulo(String titulo) {this.titulo = titulo;}
 
     public String getAutor() {return autor;}
-
     public void setAutor(String autor) {this.autor = autor;}
 
     public int getTotPaginas() {return totPaginas;}
-
     public void setTotPaginas(int totPaginas) {this.totPaginas = totPaginas;}
 
     public int getPagAtual() {return pagAtual;}
-
     public void setPagAtual(int pagAtual) {this.pagAtual = pagAtual;}
 
     public boolean isAberto() {return aberto;}
-
     public void setAberto(boolean aberto) {this.aberto = aberto;}
 
-    public String getLeitor() {return leitor;}
+    public Pessoa getLeitor() {return leitor;}
+    public void setLeitor(Pessoa leitor) {this.leitor = leitor;}
 
-    public void setLeitor(String leitor) {this.leitor = leitor;}
+    // [METODOS ABSTRATOS]
+    @Override
+    public void abrir() {this.aberto = true;}
+
+    @Override
+    public void fechar() {this.aberto = false;}
+
+    @Override
+    public void folhear(int p) {this.pagAtual = p;}
+
+    @Override
+    public void avancarPag() {this.pagAtual++;}
+
+    @Override
+    public void voltarPag() {this.pagAtual--;}
 }
