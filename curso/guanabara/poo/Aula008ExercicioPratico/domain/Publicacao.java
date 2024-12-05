71a8e8807044c45bf45fe099d535009d586023a1
William Galvão <Liam_draven@outlook.com>
2024-12-05 18:57:56 -0300

    Create Publicacao.java

---
+++ curso/guanabara/poo/Aula008ExercicioPratico/domain/Publicacao.java
@@ -0,0 +1,9 @@
+package curso.guanabara.poo.Aula008ExercicioPratico.domain;
+
+public interface Publicacao {
+    public void abrir();
+    public void fechar();
+    public void folhear(int p);
+    public void avancarPag();
+    public void voltarPag();
+}
