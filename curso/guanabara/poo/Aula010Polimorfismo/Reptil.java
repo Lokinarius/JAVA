44908b143d6b33e6ae0f438b78ade3799cb53cc5
William Galvão <Liam_draven@outlook.com>
2024-12-29 17:30:27 -0300

    Update Reptil.java

--- curso/guanabara/poo/Aula010Polimorfismo/Reptil.java
+++ curso/guanabara/poo/Aula010Polimorfismo/Reptil.java
@@ -1,26 +1,23 @@
 package curso.guanabara.poo.Aula010Polimorfismo;
 
 public class Reptil extends Animal {
-    //Atributos
+    //[ATRIBUTOS]
     private char corEscama;
 
-    //Overrride
+    //[SOBREPOSIÇÃO]
 
     @Override
     public void locomover() {
-        super.locomover();
         System.out.println("Rastejando");
     }
 
     @Override
     public void alimentar() {
-        super.alimentar();
         System.out.println("Comendo pequenos animais");
     }
 
     @Override
     public void emitirSom() {
-        super.emitirSom();
         System.out.println("Som de Réptil");
     }
 }
