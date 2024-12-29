45a2e9bfe217b51213f3abd76aca9fffea365411
William Galvão <Liam_draven@outlook.com>
2024-12-29 04:29:22 -0300

    Create Animal.java

---
+++ curso/guanabara/poo/Aula010Polimorfismo/Animal.java
@@ -0,0 +1,24 @@
+package curso.guanabara.poo.Aula010Polimorfismo;
+
+public abstract class Animal {
+    //[ATRIBUTOS]
+    protected float peso;
+    protected int idade;
+    protected int membros;
+
+    //[METODOS]
+    public void locomover(){
+
+    }
+
+    public void alimentar(){
+
+    }
+
+    public void emitirSom(){
+
+    }
+
+    //[GETTERS E SETTERS]
+
+}
