f2cd4613c41108cefb46505cdb9d4fe9c78778e5
William Galvão <Liam_draven@outlook.com>
2024-12-31 18:17:41 -0300

    Create Main011.java

---
+++ curso/guanabara/poo/Aula011PolimorfismoParte2/Main011.java
@@ -0,0 +1,13 @@
+package curso.guanabara.poo.Aula011PolimorfismoParte2;
+
+public class Main011 {
+    public static void main(String[] args) {
+        //[ENTRADA]
+        Cachorro c = new Cachorro();
+
+        //[SAÍDA]
+        c.reagir("Olá");
+        c.reagir(true);
+        c.reagir(18,22);
+    }
+}
