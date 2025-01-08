9ac2c2dea853a5ac862712cf27fd43020e0e1ffc
William Galvão <Liam_draven@outlook.com>
2025-01-08 17:28:02 -0300

    Create AcoesVideo.java

---
+++ curso/guanabara/poo/Aula012ExercioPratico/domain/AcoesVideo.java
@@ -0,0 +1,8 @@
+package curso.guanabara.poo.Aula012ExercioPratico.domain;
+
+public interface AcoesVideo {
+    //[METODOS]
+    public void play();
+    public void pause();
+    public void like();
+}
