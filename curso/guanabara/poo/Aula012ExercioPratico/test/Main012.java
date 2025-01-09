450e912b2c3adf523dab2de28808e1f3eac29598
William Galvão <Liam_draven@outlook.com>
2025-01-09 19:08:42 -0300

    Update Main012.java

--- curso/guanabara/poo/Aula012ExercioPratico/test/Main012.java
+++ curso/guanabara/poo/Aula012ExercioPratico/test/Main012.java
@@ -6,14 +6,21 @@ import curso.guanabara.poo.Aula012ExercioPratico.domain.Video;
 public class Main012 {
     public static void main(String[] args) {
         Video v[] = new Video[3];
-        v[0] = new Video("Aula 1 de POO");
-        v[1] = new Video("Aula 12 de Java");
-        v[2] = new Video("Aula 5 de IA");
+        v[0] = new Video("Aula de Sonegação");
+        v[1] = new Video("Aula de Armamento nuclear");
+        v[2] = new Video("Aula de Espalhar mentiras na net");
 
         Gafanhoto g[] = new Gafanhoto[2];
         g[0] = new Gafanhoto("Doomer",29,"M","Palhaço Triste");
         g[1] = new Gafanhoto("Egrilo",22,"F","Muie na Net");
 
+        System.out.println("\nVIDEOS\n---------------------");
         System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("\nGAFANHOTOS\n-----------------");
+        System.out.println(g[0].toString());
+        System.out.println(g[1].toString());
+
     }
 }
