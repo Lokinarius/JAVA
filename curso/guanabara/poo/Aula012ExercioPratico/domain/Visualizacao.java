4081dc6e0873c0f4fb3d2f349a9fc6855cacb245
William Galvão <Liam_draven@outlook.com>
2025-01-09 19:08:37 -0300

    Create Visualizacao.java

---
+++ curso/guanabara/poo/Aula012ExercioPratico/domain/Visualizacao.java
@@ -0,0 +1,22 @@
+package curso.guanabara.poo.Aula012ExercioPratico.domain;
+
+public class Visualizacao {
+    //ATRIBUTOS
+    private Gafanhoto espectador;
+    private Video filme;
+
+    public void avaliar(){
+
+    }
+    public void avaliar(nota){
+
+    }
+
+    //GETTER E SETTERS
+
+    public Gafanhoto getEspectador() {return espectador;}
+    public void setEspectador(Gafanhoto espectador) {this.espectador = espectador;}
+
+    public Video getFilme() {return filme;}
+    public void setFilme(Video filme) {this.filme = filme;}
+}
