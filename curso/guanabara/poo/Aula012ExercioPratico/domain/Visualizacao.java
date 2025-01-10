ee71e8b88972e0ea07b0f5c3bc94e0e562d96a3c
William Galvão <Liam_draven@outlook.com>
2025-01-10 19:10:22 -0300

    Update Visualizacao.java

--- curso/guanabara/poo/Aula012ExercioPratico/domain/Visualizacao.java
+++ curso/guanabara/poo/Aula012ExercioPratico/domain/Visualizacao.java
@@ -1,19 +1,42 @@
 package curso.guanabara.poo.Aula012ExercioPratico.domain;
 
 public class Visualizacao {
-    //ATRIBUTOS
+    //[ATRIBUTOS]
     private Gafanhoto espectador;
     private Video filme;
 
+    //[CONSTRUTOR]
+    public Visualizacao(Gafanhoto espectador, Video filme) {
+        this.espectador = espectador;
+        this.filme = filme;
+        this.espectador.setTotAssistido(this.espectador.getTotAssistido() + 1);
+        this.filme.setViews(this.filme.getViews() + 1);
+    }
+
+    //[METODOS]
     public void avaliar(){
-
-    }
-    public void avaliar(nota){
-
+        this.filme.setAvaliacao(5);
     }
 
-    //GETTER E SETTERS
+    public void avaliar(int nota){
+        this.filme.setAvaliacao(nota);
+    }
 
+    public void avaliar(double porc){
+        int tot = 0;
+        if (porc <= 20){
+            tot = 3;
+        }else if(porc <= 50){
+            tot = 5;
+        }else if(porc <= 90){
+            tot = 8;
+        }else{
+            tot = 10;
+        }
+        this.filme.setAvaliacao(tot);
+    }
+
+    //[GETTER E SETTERS]
     public Gafanhoto getEspectador() {return espectador;}
     public void setEspectador(Gafanhoto espectador) {this.espectador = espectador;}
 
